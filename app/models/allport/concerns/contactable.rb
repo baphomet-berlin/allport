746842cd5891df7660f376cc80e213bcaeea7d26
Claudio Brandolino <cbrandolino@gmail.com>
2016-03-10 14:06:40 +0100

    Added default .contact to Contactable

--- app/models/allport/concerns/contactable.rb
+++ app/models/allport/concerns/contactable.rb
@@ -5,6 +5,14 @@ module Allport
       included do
         has_one :contact, as: :contactable, class_name: Allport::Contact
         accepts_nested_attributes_for :contact
+        after_initialize :hook_contact
+
+        private
+
+        def hook_contact
+          self.contact ||= Allport::Contact.new
+        end
+
       end
     end
   end
