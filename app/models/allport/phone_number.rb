99cd14ada3b7c7eac5d65859d03e2244c36eaa0c
Claudio Brandolino <cbrandolino@gmail.com>
2016-03-10 17:25:24 +0100

    Added number splitting functionality

--- app/models/allport/phone_number.rb
+++ app/models/allport/phone_number.rb
@@ -6,5 +6,12 @@ module Allport
     belongs_to :contact
     validates_plausible_phone :number, presence: true
     phony_normalize :number, default_country_code: 'US'
+    after_validate :split_number
+
+    def split_number
+      split_number = Phony.split(self.number.gsub(/\D/, ''))
+      self.prefix = split_number.shift
+      self.number = split_number.join
+    end
   end
 end
