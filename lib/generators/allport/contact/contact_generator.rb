e00645193638d6542c646d762f72280c8dcdaef2
Claudio Brandolino <cbrandolino@gmail.com>
2016-03-16 06:48:08 +0100

    Added contact migration

--- lib/generators/allport/contact/contact_generator.rb
+++ lib/generators/allport/contact/contact_generator.rb
@@ -1,5 +1,23 @@
 require 'rails/generators/active_record'
 
-class Allport::ContactGenerator < Rails::Generators::NamedBase
-  source_root File.expand_path('../templates', __FILE__)
+class Allport::ContactGenerator < ActiveRecord::Generators::Base
+
+  desc "Create a migration to add contactable-specific fields to your model. " +
+       "The NAME argument is the name of your model"
+
+  def self.source_root
+    @source_root ||= File.expand_path('../templates', __FILE__)
+  end
+
+  def generate_migration
+    self.migration_template "contactable_migration.rb.erb", "db/migrate/#{migration_file_name}"
+  end
+
+  def migration_name
+    "add_allport_contact_to_#{name.underscore.pluralize}"
+  end
+
+  def migration_file_name
+    "#{migration_name}.rb"
+  end
 end
