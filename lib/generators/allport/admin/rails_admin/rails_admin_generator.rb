db5e3ccb7b7cd1c391b140802f5cd2fd834e359e
Claudio Brandolino <cbrandolino@gmail.com>
2016-03-17 13:36:50 +0100

    Added generator template for rails admin

--- lib/generators/allport/admin/rails_admin/rails_admin_generator.rb
+++ lib/generators/allport/admin/rails_admin/rails_admin_generator.rb
@@ -1,7 +1,14 @@
 module Allport
   module Admin
-    class RailsAdminGenerator < Rails::Generators::NamedBase
-      source_root File.expand_path('../templates', __FILE__)
+    class RailsAdminGenerator < Rails::Generators::Base
+
+      def self.source_root
+        @source_root ||= File.expand_path('../templates', __FILE__)
+      end
+
+      def copy_initializer_file
+        template "allport_rails_admin.rb.erb", "config/initializers/allport_rails_admin.rb"
+      end    
     end
   end
 end
